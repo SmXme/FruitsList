7837973afcf45108fe4b31acd9b6ebeb583876b9
SmXme <pillot.clement@gmail.com>
2017-10-31 14:41:18 +0100

    Cleaning the fruit list at the start of the showList function

--- index.php
+++ index.php
@@ -14,20 +14,23 @@
 		<script>
 			var fruits = []
 			function addFruits() {
-				document.getElementById('fruitsList').innerHTML = "";
 				fruits.push(document.getElementById('inputFruits').value);
 				showList();
 			}
+
 				function deleteThis(nb){
 				fruits.splice(nb,1);
 				showList();
 			}
+
+
 			function showList(){
 				document.getElementById('fruitsList').innerHTML = "";
 				for (j = 0; j < fruits.length ; j++){
 				document.getElementById('fruitsList').innerHTML += '<li id="'+fruits[j]+'">'+fruits[j]+'<input type="button" value = "Supprimer" name="inputDelete" onclick ="deleteThis('+ j +');" id="inputDelete"></li>';
 				}
 			}
+
 			function debug(){
 				console.log(fruits);
 			}
