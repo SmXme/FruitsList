5a2448db3f90463f33510ad51ca1a76a33b16d1a
SmXme <pillot.clement@gmail.com>
2017-10-31 11:59:53 +0100

    Created the function who add a fruits and logs the fruits array

--- index.php
+++ index.php
@@ -7,8 +7,17 @@
 	</head>
 	<body class="text-center">
 		<div class="container">
-
+			<input type="text" name="inputFruits" id="inputFruits">
+			<input type="button" value = "Ajouter Fruit" name="inputButton" onclick="addFruits();">
+			<ul id="fruitsList"></ul>
 			<script>
+				var fruits = []
+				var laListe = document.getElementById('fruitsList').innerHTML;
+
+				function addFruits() {
+					fruits.push(document.getElementById('inputFruits').value);
+					console.log(fruits);
+				}
 			</script>
 		</div>
 		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
