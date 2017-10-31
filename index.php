594ea11e054b3d5dc7bc123ee183df0507a3e23c
SmXme <pillot.clement@gmail.com>
2017-10-31 12:10:14 +0100

    Modifie the function to parse an array and list elements

--- index.php
+++ index.php
@@ -9,19 +9,20 @@
 		<div class="container">
 			<input type="text" name="inputFruits" id="inputFruits">
 			<input type="button" value = "Ajouter Fruit" name="inputButton" onclick="addFruits();">
-			<ul id="fruitsList"></ul>
-			<script>
-				var fruits = []
-				var laListe = document.getElementById('fruitsList').innerHTML;
-
-				function addFruits() {
-					fruits.push(document.getElementById('inputFruits').value);
-					console.log(fruits);
+		<ul id="fruitsList"></ul>
+		<script>
+			var fruits = []
+			function addFruits() {
+				document.getElementById('fruitsList').innerHTML = "";
+				fruits.push(document.getElementById('inputFruits').value);
+				for (i = 0; i < fruits.length ; i++){
+					document.getElementById('fruitsList').innerHTML += "<li>"+fruits[i]+'</li>';
 				}
-			</script>
-		</div>
-		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-	</body>
+			}
+		</script>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</body>
 </html>
